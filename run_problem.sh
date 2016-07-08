3c9d63ecf71b5e81fe321da37d0ec6a7ad28289a
Nathaniel Saul <nat@saulgill.com>
2016-07-08 00:39:24 -0700

    Comments of script

--- run_problem.sh
+++ run_problem.sh
@@ -3,6 +3,9 @@
 PROBLEM=$1
 echo Begin setup for problem $PROBLEM
 
+# Convert the original Solomon Problem dataset into a set saved using our structs.
 python3 src/data/make_dataset.py data/raw/$PROBLEM.txt data/interim/$PROBLEM.p
+
+# Plot the dataset with the depot highlighted.
 python3 src/visualization/visualize.py data/interim/$PROBLEM.p
 

fecdd6184e6014fdbe831eabb41149cac8f0c3f9
Nathaniel Saul <nat@saulgill.com>
2016-07-14 22:38:27 -0700

    time windows, multiple routes functional here.

--- run_problem.sh
+++ run_problem.sh
@@ -10,34 +10,13 @@ fi
 
 echo Begin setup for problem $PROBLEM
 
-
-
-# Convert the problem to usable format
-
-
 # Convert the original Solomon Problem dataset into a set saved using our structs.
 python3 src/data/make_dataset.py data/raw/$PROBLEM.txt data/interim/$PROBLEM.p
-
 
 # Run all tests
 python3 src/test/TestMatrices.py
 python3 src/test/TestSolomonProblem.py
 python3 src/test/TestCostFunction.py
-#python3 src/prototype/TestRouteConstructionAlgorithm.py
-#python3 src/prototype/TestAuxiliaryAlgorithm.py
-
 
 python3 src/main/basicOps.py data/interim/$PROBLEM.p
-
-
-#python3 src/prototype/routeConstructionAlgorithm.py data/interim/$PROBLEM.p
-#python3 src/prototype/auxiliaryAlgorithm.py data/interim/$PROBLEM.p
-#mypy src/prototype/routeConstructionAlgorithm.py data/interim/$PROBLEM.p
-
-
-
-# Plot the dataset with the depot highlighted.
-#python3 src/visualization/visualize.py data/interim/$PROBLEM.p
-
-# Generate a route
 

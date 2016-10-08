d87dff734bd813a3fbf303d8735fc260d6c7e964
Nathaniel Saul <nat@saulgill.com>
2016-10-08 01:16:05 -0700

    rearranging tests

---
+++ src/test/Test_Heuristic.py
@@ -0,0 +1,26 @@
+import unittest
+
+import src
+
+
+class Test_Heuristic(unittest.TestCase):
+    def setUp(self):
+        
+        c = src.Customer(0,1,1,1,1,1)        
+         
+        
+
+
+    def test_failing(self):
+        h = src.Heuristic
+        
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ src/test/__init__.py
@@ -0,0 +1,25 @@
+
+import unittest
+
+from src.test.Algorithms.TestCostFunction import TestCostFunction
+from src.test.Algorithms.TestNextFinder import TestNextFinder
+from src.test.Algorithms.TestImprovement import TestImprovement
+from src.test.Algorithms.TestHeuristic import TestHeuristic
+from src.test.Algorithms.TestRollOut import TestRollOut
+from src.test.Algorithms.TestValidator import TestValidator
+
+''' Stub for new tests
+import unittest
+
+class TestHeuristic(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_stub(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+'''
+
+
+

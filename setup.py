0c3b9f6eb3f246755d07851d95d70f98e6b5e600
Nathaniel Saul <nat@saulgill.com>
2016-11-08 05:39:59 +0000

    nice print statements

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+print("setup ICRI project")
 import os
 import db.setup
 
+print("build interim storage dirs")
 dirs = 'data/solutions/search'
 if not os.path.isdir(dirs):
 	os.makedirs(dirs)

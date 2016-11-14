ebb014bd2a6fa092ba433e4cb2edeadc1e219ab7
Nathaniel Saul <nat@saulgill.com>
2016-11-13 17:59:39 -0800

    tools to merge results

--- db/queries.py
+++ db/queries.py
@@ -1,16 +1,36 @@
+import functools
+import pandas as pd
+from operator import add
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from db.setup import Result
 
+from db.setup import Result
 from src import Heuristic, Search, RollOut
 
-engine = create_engine('sqlite:///db/results.db')
-Session = sessionmaker(bind=engine)
-session = Session()
+tab_root = 'sqlite:///db/'
+tables = ['results.db', 'results/gcloud_r.db', 'results/gcloud_results.db',
+          'results/gcloud_results_large.db', 'results/other_results.db',
+          'results/unknown_params_results.db']
 
-#def remove_duplicates():
-   
+def build_session(name):
+    engine = create_engine(name)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def aggregate_results(tables):
+    def get_records(t):
+        session = build_session(tab_root + t)
+        results = get_results(session)
+        data_records = [rec.__dict__ for rec in results.all()]
+        return data_records
+
+    results_list = map(get_records, tables)
+    results = functools.reduce(add, results_list, [])
+    df = pd.DataFrame.from_records(results)
+    return df
+
 
 
 def params_in(params):
@@ -32,7 +52,16 @@ def print_results():
     for result in get_results():
         print(result)
 
-def get_results():
+def get_results(session):
     return session.query(Result)
 
+if __name__=='__main__':
+    res = aggregate_results(tables)
+    res = res.drop('id', 1).drop('_sa_instance_state', 1).drop_duplicates()
+
+    best = res.loc[res.groupby('problem')['num_vehicles', 'total_distance'].idxmin()]
+    best['p_groups'] = best['problem'].map(lambda x: x.zfill(5)[:3])
+
+    avg = best.groupby('p_groups')['num_vehicles'].mean()
+    print(res)
 
--- db/setup.py
+++ db/setup.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
- 
+
 class Result(Base):
     ''' Main results workhorse for cateloging and reporting on all runs '''
     __tablename__ = 'results'
@@ -14,28 +14,28 @@ class Result(Base):
     num_vehicles = Column(Integer)
     total_distance = Column(Float)
     vehicles = Column(String) # nosql like, just for storage
-    solution_string = Column(String)    
-    
+    solution_string = Column(String)
+
     width = Column(Integer)
     depth = Column(Integer)
     count = Column(Integer)
-    
+
     search_type = Column(String)
     heuristic_type = Column(String)
     rollout_type = Column(String)
     algo_type = Column(String)
     run_type = Column(String)
-    
+
     d0 = Column(Float)
     d1 = Column(Float) # these probably aren't necessary?
     d2 = Column(Float)
     d3 = Column(Float)
     d4 = Column(Float)
-    
+
     def __repr__(self):
-        return "<Result(problem=%s', (%s, %s), (%s, %s, %s), (%s, %s) [%s]>"%(self.problem, 
-                    self.algo_type, self.run_type, 
-                    self.search_type, self.heuristic_type, self.rollout_type, 
+        return "<Result(problem=%s', (%s, %s), (%s, %s, %s), (%s, %s) [%s]>"%(self.problem,
+                    self.algo_type, self.run_type,
+                    self.search_type, self.heuristic_type, self.rollout_type,
                     self.num_vehicles, self.total_distance, [self.d0, self.d1,
                         self.d2, self.d3, self.d4])
 
